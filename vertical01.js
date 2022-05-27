35d1c6f91cd68aa2954098439bf7f2eacda86c29
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-05-27 11:48:06 -0400

    first commit

---
+++ vertical01.js
@@ -0,0 +1,16 @@
+// create a function that will output the letters of each name vertically
+
+let classmateNames = ["Samuel", "Hannah", "Tom", "Grethel", "Mathias", "Tanya", "Art", "Derrel", "Ben" ]
+let emptyArray = []
+
+function classmateSorting (emptyArray) {
+for (i=0; i < classmateNames.length; i++) {
+    for (let j=[0]; j < classmateNames[i].length; j++) {
+        emptyArray=(classmateNames[i][j])
+        console.log(emptyArray);
+    }
+    if (j=[0]){
+        console.log("\n")
+    }
+}}
+classmateSorting(emptyArray)
